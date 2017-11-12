6f5ef6c0896eeb07124f328e8103dc9e0e3ea1f9
Joseph Widjaja <jewidjaj@ucsd.edu>
2017-11-11 23:03:13 -0800

    implemented recommender stuff we discussed

--- Student.py
+++ Student.py
@@ -3,3 +3,6 @@ class Student(object):
     def __init__(self, n, ct):
         self.name = n
         self.concept_table = ct #concept to one hot encoded profiling questions
+
+    def updateConcept(self, cname, l):
+        self.concept_table[cname] = l
--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from Student import *
 from Concept import *
 from ConceptGraph import *
 from Question import *
-import random as rand
 import numpy as np
 
 def createDummyConcept(name):
@@ -25,9 +24,40 @@ def createDummyStudent(name, conceptgraph):
 
     return s
 
-# Todo
-def recommendConcept():
-    return
+# input: all students, the struggling student, the concept wrong
+# output: best concept for that student to review
+def recommendConcept(students, stu, cwrong):
+    matched = gatherConceptTables(students, stu, cwrong)
+
+    conceptsToConsider = [c.name for c in cwrong.prev_concepts]
+    conceptsToConsider = conceptsToConsider + [cwrong.name]
+
+    zeroCounts = [zeroCounter(m, conceptsToConsider) for m in matched]
+
+    zeroCounts = np.array(zeroCounts)
+    zeroCounts = zeroCounts.T
+
+    means = [np.mean(z) for z in zeroCounts]
+
+    ind = means.index(max(means))
+
+    return conceptsToConsider[ind]
+
+def gatherConceptTables(students, stu, cwrong):
+    matched = [students[s].concept_table for s in students if students[s].concept_table[cwrong.name] == stu.concept_table[cwrong.name] and students[s] is not stu]
+
+    return matched
+
+def zeroCounter(m, considered):
+
+    zeroCounts = []
+
+    for c in considered:
+        zeroCounts.append(m[c].count(0))
+
+    return zeroCounts
+
+
 
 # input: concept
 # output: RecQuestion with highest help_rating
@@ -52,3 +82,19 @@ conceptgraph.connectConcept(conceptgraph.concepts["c4"],conceptgraph.concepts["c
 students = {}
 for i in range(1, 5):
     students["s" + str(i)] = createDummyStudent("s" + str(i), conceptgraph)
+
+l1 = [1, 0, 1, 0]
+l2 = [0, 1, 0, 0]
+l3 = [0, 1, 1, 0]
+
+for s in students:
+    students[s].updateConcept("c1", l1)
+    students[s].updateConcept("c2", l2)
+    students[s].updateConcept("c4", l3)
+
+# test out stuff
+# recommend concept based on number of 0s from other students and concept's neighbors
+bestconcept = recommendConcept(students, students["s2"], conceptgraph.concepts["c3"])
+print(bestconcept)
+# recommend question with highest rating
+print(recommendQuestion(conceptgraph.concepts[bestconcept]).help_rating)
